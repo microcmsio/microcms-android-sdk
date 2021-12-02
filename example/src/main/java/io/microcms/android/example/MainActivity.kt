f30fc01302bb3e0e9a9a60e6eb1c9977adeaa92f
Ryosuke Hiramatsu <himaratsu@gmail.com>
2021-12-02 12:33:08 +0900

    WIP: add write apis

--- example/src/main/java/io/microcms/android/example/MainActivity.kt
+++ example/src/main/java/io/microcms/android/example/MainActivity.kt
@@ -2,6 +2,7 @@ package io.microcms.android.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import io.microcms.android.*
 import io.microcms.android.example.databinding.ActivityMainBinding
 
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         //リスト取得もしくはオブジェクト形式の取得
         client.get(
-            "blog",
+            "comments",
             listOf(Limit(2), Filters("createdAt[greater_than]2021"))
         ) { result ->
             result.onSuccess { binding.listJson.text = it.toString(2) }
@@ -30,12 +31,27 @@ class MainActivity : AppCompatActivity() {
 
         //個別に取得
         client.get(
-            "blog",
-            "my-first-content",
+            "comments",
+            "alagb4_z8p",
             listOf(Fields(listOf("id")))
         ) { result ->
             result.onSuccess { binding.detailJson.text = it.toString((2)) }
                 .onFailure { binding.detailJson.text = it.toString() }
+        }
+
+        Log.d("himara2", "### POST-----------")
+        val params = mapOf(
+            "content" to  "Hello Android SDK",
+            "postedBy" to "Pixel 3"
+        )
+//        client.post("comments", params) { result ->
+//            result.onSuccess { Log.d("himara2", it.toString()) }
+//                    .onFailure {  Log.d("himara2", it.toString()) }
+//        }
+
+        client.put("comments", "test", params) { result ->
+            result.onSuccess { Log.d("himara2", it.toString()) }
+                    .onFailure {  Log.d("himara2", it.toString()) }
         }
     }
 }
--- sdk/src/main/java/io/microcms/android/MicrocmsClient.kt
+++ sdk/src/main/java/io/microcms/android/MicrocmsClient.kt
@@ -1,6 +1,7 @@
 package io.microcms.android
 
 import android.os.Looper
+import android.util.Log
 import androidx.core.os.HandlerCompat
 import org.json.JSONObject
 import java.net.HttpURLConnection
@@ -33,6 +34,119 @@ class MicrocmsClient(
         return JSONObject(inputAsString)
     }
 
+    private fun doPostRequest(
+            endpoint: String,
+            params: Map<String, Any>
+    ): JSONObject {
+        Log.d("himara2", "### doPostRequest -----------")
+        val bodyData = JSONObject(params).toString().toByteArray()
+
+        val url =
+                URL("https://${serviceDomain}.${baseDomain}/api/${apiVersion}/${endpoint}")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "POST"
+        connection.doOutput = true
+        connection.setChunkedStreamingMode(0)
+
+        connection.setRequestProperty("X-MICROCMS-API-KEY", apiKey)
+        connection.setRequestProperty("Content-type", "application/json; charset=utf-8")
+
+        val outputStream = connection.outputStream
+        outputStream.write(bodyData)
+        outputStream.flush()
+        outputStream.close()
+
+        val statusCode = connection.responseCode
+        Log.d("himara2", "### statusCode is $statusCode -----------")
+
+        val inputAsString = connection.inputStream.bufferedReader().use { it.readText() }
+        return JSONObject(inputAsString)
+    }
+
+    private fun doPutRequest(
+            endpoint: String,
+            contentId: String,
+            params: Map<String, Any>
+    ): JSONObject {
+        Log.d("himara2", "### doPutRequest -----------")
+        val bodyData = JSONObject(params).toString().toByteArray()
+
+        val url =
+                URL("https://${serviceDomain}.${baseDomain}/api/${apiVersion}/${endpoint}/${contentId}")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "PUT"
+        connection.doOutput = true
+        connection.setChunkedStreamingMode(0)
+
+        connection.setRequestProperty("X-MICROCMS-API-KEY", apiKey)
+        connection.setRequestProperty("Content-type", "application/json; charset=utf-8")
+
+        val outputStream = connection.outputStream
+        outputStream.write(bodyData)
+        outputStream.flush()
+        outputStream.close()
+
+        val statusCode = connection.responseCode
+        Log.d("himara2", "### statusCode is $statusCode -----------")
+
+        val inputAsString = connection.inputStream.bufferedReader().use { it.readText() }
+        return JSONObject(inputAsString)
+    }
+
+    private fun doPatchRequest(
+            endpoint: String,
+            contentId: String,
+            params: Map<String, Any>
+    ): JSONObject {
+        Log.d("himara2", "### doPatchRequest -----------")
+        val bodyData = JSONObject(params).toString().toByteArray()
+
+        val url =
+                URL("https://${serviceDomain}.${baseDomain}/api/${apiVersion}/${endpoint}/${contentId}")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "PATCH"
+        connection.doOutput = true
+        connection.setChunkedStreamingMode(0)
+
+        connection.setRequestProperty("X-MICROCMS-API-KEY", apiKey)
+        connection.setRequestProperty("Content-type", "application/json; charset=utf-8")
+
+        val outputStream = connection.outputStream
+        outputStream.write(bodyData)
+        outputStream.flush()
+        outputStream.close()
+
+        val statusCode = connection.responseCode
+        Log.d("himara2", "### statusCode is $statusCode -----------")
+
+        val inputAsString = connection.inputStream.bufferedReader().use { it.readText() }
+        return JSONObject(inputAsString)
+    }
+
+    private fun doDeleteRequest(
+            endpoint: String,
+            contentId: String,
+    ): JSONObject {
+        Log.d("himara2", "### doDeleteRequest -----------")
+
+        val url =
+                URL("https://${serviceDomain}.${baseDomain}/api/${apiVersion}/${endpoint}/${contentId}")
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "DELETE"
+
+        connection.setRequestProperty("X-MICROCMS-API-KEY", apiKey)
+        connection.setRequestProperty("Content-type", "application/json; charset=utf-8")
+
+        connection.connect()
+
+        val statusCode = connection.responseCode
+        Log.d("himara2", "### statusCode is $statusCode -----------")
+
+        val inputAsString = connection.inputStream.bufferedReader().use { it.readText() }
+        return JSONObject(inputAsString)
+    }
+
+
     fun get(
         endpoint: String,
         params: List<MicrocmsParameter> = listOf(),
@@ -50,6 +164,31 @@ class MicrocmsClient(
         thread {
             val result =
                 runCatching { doRequest("${endpoint}${contentId?.let { "/$it" } ?: ""}", params) }
+            mainThreadHandler.post { callback.invoke(result) }
+        }
+    }
+
+    fun post(
+        endpoint: String,
+        params: Map<String, Any> = mapOf(),
+        callback: (Result<JSONObject>) -> Unit
+    ) {
+        thread {
+            val result =
+                    runCatching { doPostRequest(endpoint, params) }
+            mainThreadHandler.post { callback.invoke(result) }
+        }
+    }
+
+    fun put(
+            endpoint: String,
+            contentId: String,
+            params: Map<String, Any> = mapOf(),
+            callback: (Result<JSONObject>) -> Unit
+    ) {
+        thread {
+            val result =
+                    runCatching { doPutRequest(endpoint, contentId, params) }
             mainThreadHandler.post { callback.invoke(result) }
         }
     }
